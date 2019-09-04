16d4076945936c783faf7e08c6c5ee63ce97f11d
yuyasaito210 <yuyasaito210@gmail.com>
2019-09-04 09:56:41 -0700

    removed comments

--- src/Nono.js
+++ src/Nono.js
@@ -1,39 +1,3 @@
-// import { Provider } from 'react-redux';
-// import React from 'react';
-// import { View, ActivityIndicator, StyleSheet } from 'react-native';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { colors } from './styles';
-
-// import { store, persistor } from './redux/store';
-
-// import AppView from './modules/AppViewContainer';
-
-// export default function Nono() {
-//   return (
-//     <Provider store={store}>
-//       <PersistGate
-//         loading={
-//           <View style={styles.container}>
-//             <ActivityIndicator color={colors.red} />
-//           </View>
-//         }
-//         persistor={persistor}
-//       >
-//         <AppView />
-//       </PersistGate>
-//     </Provider>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: 'white',
-//   },
-// });
-
 import { registerRootComponent, AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
