7f5b119f6ce949d1f1a3aab659f6974321c9dc4a
yuyasaito210 <yuyasaito210@gmail.com>
2019-09-27 15:52:56 -0700

    updated PaymentView stylesheet.

--- src/modules/profile/payment/styles.js
+++ src/modules/profile/payment/styles.js
@@ -1,31 +1,21 @@
 import { StyleSheet } from 'react-native';
-import { W, H, em } from '~/constants/Layout';
-import { bold } from 'ansi-colors';
+import { colors } from '~/styles';
 
 const styles = StyleSheet.create({
     backButton: {
-        tintColor: '#35CDFA'
-        },
-
+        tintColor: colors.primaryColor,
+    },
     paramTitle: {
         fontSize: 24, fontWeight: 'bold',
         alignItems: 'center',
     },
-
     payInfo: {
-        color: '#35CDFA',
+        color: colors.primaryColor,
         alignItems: 'center',
     },
-
-    addInfo1: {
-
-    },
-
     addInfo2: {
         color: '#9F9F9F',
     },
-
-    
 })
 
 export default styles;
